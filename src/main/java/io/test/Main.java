edc11c6503b088d566454ca7a027fcdd7a527146
namkyu1999 <lak9348@konkuk.ac.kr>
2024-10-21 20:19:17 +0900

    feat: version 1
    Signed-off-by: namkyu1999 <lak9348@konkuk.ac.kr>

--- src/main/java/io/test/Main.java
+++ src/main/java/io/test/Main.java
@@ -1,17 +1,45 @@
 package io.test;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static final String FILE_NAME = "measurements-1m.txt";
+
+    public static void main(String[] args) throws IOException {
+        HashMap<String, List<String>> cities = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] split = line.split(";");
+                if(cities.containsKey(split[0])){
+                    cities.get(split[0]).add(split[1]);
+                }else{
+                    ArrayList<String> newList = new ArrayList<>();
+                    newList.add(split[1]);
+                    cities.put(split[0], newList);
+                }
+                line = br.readLine();
+            }
+
+            List<String> results = cities.keySet().stream().sorted().map(key -> {
+                List<String>temperatures = cities.get(key);
+                Collections.sort(temperatures);
+                String min = String.format("%.1f", Double.parseDouble(temperatures.getFirst()));
+                String max = String.format("%.1f", Double.parseDouble(temperatures.getLast()));
+                String mean = String.format("%.1f", temperatures.stream().mapToDouble(Double::parseDouble).average().getAsDouble());
+                return key+"="+min+"/"+mean+"/"+max;
+            }).toList();
+
+            System.out.println("{" + String.join(", ", results) + "}");
+
+        } catch (Exception e) {
+            System.err.println(e);
+            throw e;
         }
     }
 }
