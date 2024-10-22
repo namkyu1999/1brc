f023a504a6b51c9b00c019e9d7816e1d88ef6c49
namkyu1999 <lak9348@konkuk.ac.kr>
2024-10-22 15:34:08 +0900

    feat: version 2
    Signed-off-by: namkyu1999 <lak9348@konkuk.ac.kr>

--- src/main/java/io/test/Main.java
+++ src/main/java/io/test/Main.java
@@ -6,32 +6,41 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static final String FILE_NAME = "measurements-1m.txt";
+    public static final String FILE_NAME = "measurements-1b.txt";
 
     public static void main(String[] args) throws IOException {
-        HashMap<String, List<String>> cities = new HashMap<>();
+        HashMap<String, Double[]> cities = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
             String line = br.readLine();
 
             while (line != null) {
                 String[] split = line.split(";");
-                if(cities.containsKey(split[0])){
-                    cities.get(split[0]).add(split[1]);
-                }else{
-                    ArrayList<String> newList = new ArrayList<>();
-                    newList.add(split[1]);
+                Double value = Double.parseDouble(split[1]);
+
+                if(!cities.containsKey(split[0])){
+                    Double[] newList = new Double[4];
+                    newList[0] = Double.MAX_VALUE;
+                    newList[1] = 0.0;
+                    newList[2] = Double.MIN_VALUE;
+                    newList[3] = 0.0;
                     cities.put(split[0], newList);
                 }
+                Double[] arr = cities.get(split[0]);
+
+                if (arr[0] > value) arr[0] = value;
+                else if (arr[2] < value) arr[2] = value;
+                arr[1] += value;
+                arr[3] += 1;
+
                 line = br.readLine();
             }
 
             List<String> results = cities.keySet().stream().sorted().map(key -> {
-                List<String>temperatures = cities.get(key);
-                Collections.sort(temperatures);
-                String min = String.format("%.1f", Double.parseDouble(temperatures.getFirst()));
-                String max = String.format("%.1f", Double.parseDouble(temperatures.getLast()));
-                String mean = String.format("%.1f", temperatures.stream().mapToDouble(Double::parseDouble).average().getAsDouble());
+                Double[] temperatures = cities.get(key);
+                String min = String.format("%.1f", temperatures[0]);
+                String max = String.format("%.1f", temperatures[2]);
+                String mean = String.format("%.1f", temperatures[1] / temperatures[3]);
                 return key+"="+min+"/"+mean+"/"+max;
             }).toList();
 
