c4b7f5a937f9c7793d05d7500792fc4df286ddba
namkyu1999 <lak9348@konkuk.ac.kr>
2024-10-23 16:06:00 +0900

    feat: version 3
    Signed-off-by: namkyu1999 <lak9348@konkuk.ac.kr>

--- src/main/java/io/test/Main.java
+++ src/main/java/io/test/Main.java
@@ -1,54 +1,52 @@
 package io.test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static final String FILE_NAME = "measurements-1b.txt";
 
     public static void main(String[] args) throws IOException {
-        HashMap<String, Double[]> cities = new HashMap<>();
+        try (Stream<String> lines = java.nio.file.Files.lines(Paths.get(FILE_NAME))) {
+            HashMap<String, Double[]> map = lines
+                    .map(line -> line.split(";"))
+                    .reduce(
+                            new HashMap<>(),
+                            (hashMap, e) -> {
+                                Double value = Double.parseDouble(e[1]);
+                                if(!hashMap.containsKey(e[0])) {
+                                    Double[] newList = new Double[4];
+                                    newList[0] = Double.MAX_VALUE;
+                                    newList[1] = 0.0;
+                                    newList[2] = Double.MIN_VALUE;
+                                    newList[3] = 0.0;
+                                    hashMap.put(e[0], newList);
+                                }
+                                Double[] arr = hashMap.get(e[0]);
+                                if (arr[0] > value) arr[0] = value;
+                                else if (arr[2] < value) arr[2] = value;
+                                arr[1] += value;
+                                arr[3] += 1;
+                                return hashMap;
+                            },
+                            (m, m2) -> {
+                                m.putAll(m2);
+                                return m;
+                            }
+                    );
 
-        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
-            String line = br.readLine();
-
-            while (line != null) {
-                String[] split = line.split(";");
-                Double value = Double.parseDouble(split[1]);
-
-                if(!cities.containsKey(split[0])){
-                    Double[] newList = new Double[4];
-                    newList[0] = Double.MAX_VALUE;
-                    newList[1] = 0.0;
-                    newList[2] = Double.MIN_VALUE;
-                    newList[3] = 0.0;
-                    cities.put(split[0], newList);
-                }
-                Double[] arr = cities.get(split[0]);
-
-                if (arr[0] > value) arr[0] = value;
-                else if (arr[2] < value) arr[2] = value;
-                arr[1] += value;
-                arr[3] += 1;
-
-                line = br.readLine();
-            }
-
-            List<String> results = cities.keySet().stream().sorted().map(key -> {
-                Double[] temperatures = cities.get(key);
-                String min = String.format("%.1f", temperatures[0]);
-                String max = String.format("%.1f", temperatures[2]);
-                String mean = String.format("%.1f", temperatures[1] / temperatures[3]);
-                return key+"="+min+"/"+mean+"/"+max;
-            }).toList();
-
-            System.out.println("{" + String.join(", ", results) + "}");
-
-        } catch (Exception e) {
-            System.err.println(e);
-            throw e;
+            System.out.println(
+                    map.keySet().stream().sorted().map(key -> {
+                        Double[] temperatures = map.get(key);
+                        String min = String.format("%.1f", temperatures[0]);
+                        String max = String.format("%.1f", temperatures[2]);
+                        String mean = String.format("%.1f", temperatures[1] / temperatures[3]);
+                        return key+"="+min+"/"+mean+"/"+max;
+                    }).collect(Collectors.joining(", ", "{", "}"))
+            );
         }
     }
 }
